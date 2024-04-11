78cf4f891c4e113f40e69e586c4058bf25d3b596
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-04-10 23:26:52 -0300

    first commit

---
+++ src/main/java/br/com/fiap/techchallenger4/logisticaentrega/LogisticaEntregaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techchallenger4.logisticaentrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticaEntregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticaEntregaApplication.class, args);
+	}
+
+}
