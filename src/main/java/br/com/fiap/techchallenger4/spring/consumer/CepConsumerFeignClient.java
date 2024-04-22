e2c3f7b96d98d34fc1ef621e7c1d2db0fe5bb5e6
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-04-21 22:08:20 -0300

    url endpoint consumer cep movido para application

--- src/main/java/br/com/fiap/techchallenger4/spring/consumer/CepConsumerFeignClient.java
+++ src/main/java/br/com/fiap/techchallenger4/spring/consumer/CepConsumerFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.fiap.techchallenger4.logisticaentrega.dominio.entities.geo.CEP;
 
-@FeignClient(name = "buscarCepExterno", url="https://cep.awesomeapi.com.br/json")
+@FeignClient(name = "buscarCepExterno", url="${url.cep.consumer.feign.client}")
 public interface CepConsumerFeignClient {
     
     @GetMapping("/{cep}")
